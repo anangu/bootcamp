8391df78d254edce6fe3453252ec6e53264c599c
anangu <anangu.bt@gmail.com>
2021-06-28 15:21:22 -0500

    Primitive Type Long and Short

--- current-java-masterclass-pgm/PrimitiveType/src/com/company/Main.java
+++ current-java-masterclass-pgm/PrimitiveType/src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	// write your code here
         short shortValue = 10;
         byte  byteValue = 20;
-        int intValue = 30;
+        int intValue = 40;
         long longValue = 10000L + 20L * (shortValue + byteValue + intValue);
         System.out.println (longValue);
         short shortTotal = (short) (1000 * 10 * (shortValue + byteValue + intValue));
