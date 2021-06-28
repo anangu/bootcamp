38c31e36ac5392302e7da5455550a3b80e727582
anangu <anangu.bt@gmail.com>
2021-06-28 15:16:46 -0500

    Initial Commit

--- current-java-masterclass-pgm/PrimitiveType/src/com/company/Main.java
+++ current-java-masterclass-pgm/PrimitiveType/src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        short shortValue = 10;
+        byte  byteValue = 20;
+        int intValue = 30;
+        long longValue = 10000L + 20L * (shortValue + byteValue + intValue);
+        System.out.println (longValue);
+        short shortTotal = (short) (1000 * 10 * (shortValue + byteValue + intValue));
+        System.out. println (shortTotal);
     }
 }
