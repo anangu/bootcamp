e87cb470bc1923506c8a36e13d1fcf0251ac7bf1
anangu <anangu.bt@gmail.com>
2021-07-01 17:40:32 -0500

    Float&Double

--- current-java-masterclass-pgm/PrimitiveType/src/com/company/Main.java
+++ current-java-masterclass-pgm/PrimitiveType/src/com/company/Main.java
@@ -3,13 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code
         short shortValue = 10;
-        byte  byteValue = 20;
+        byte byteValue = 20;
         int intValue = 40;
-        long longValue = 10000L + 20L * (shortValue + byteValue + intValue);
-        System.out.println (longValue);
-        short shortTotal = (short) (1000 * 10 * (shortValue + byteValue + intValue));
-        System.out. println (shortTotal);
+        long longValue = 10000L + 20L * (long)(shortValue + byteValue + intValue);
+        System.out.println(longValue);
+        short shortTotal = (short)(10000 * (shortValue + byteValue + intValue));
+        System.out.println(shortTotal);
+
     }
 }
