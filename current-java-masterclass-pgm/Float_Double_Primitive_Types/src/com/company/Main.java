7da4a4fd354178fd21ca6617c4c6171c90202054
anangu <anangu.bt@gmail.com>
2021-07-01 17:43:15 -0500

    Float&Double Done

---
+++ current-java-masterclass-pgm/Float_Double_Primitive_Types/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float myMinFloatValue = Float.MIN_VALUE;
+	float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Min FLoat Val ="+ myMinFloatValue);
+        System.out.println("Max FLoat val ="+ myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Min double Val ="+ myMinDoubleValue);
+        System.out.println("Max double val ="+ myMaxDoubleValue);
+
+    }
+}
